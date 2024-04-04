fc2d05337b6a70c12b7a7586954de9870d6e6f13
Archangel2903 <archangel2903@gmail.com>
2024-04-04 17:07:42 +0300

    fix git

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import '../scss/main.scss';
 import $ from 'jquery';
+import 'jquery-ui'
+import 'jquery-ui/ui/effect';
+import 'jquery-ui/ui/widgets/tabs';
 import 'bootstrap';
 import 'popper.js';
 import select2 from 'select2/dist/js/select2.min';
-// import '../img/chevrone.svg';
+import intlTelInput from 'intl-tel-input';
 
 $(window).on('load', function () {
     let b = $('body');
@@ -19,6 +22,59 @@ $(window).on('load', function () {
 });
 
 $(function () {
+    // Password switch
+    const passBtn = document.querySelector('.form-button-pass');
+    let inputs = document.querySelectorAll('input[type="password"]');
+    passBtn.addEventListener('click', function () {
+        if (inputs[0].type === 'password') {
+            inputs.forEach(function(input) {
+                input.type = 'text';
+            });
+            this.innerHTML = '<svg><use xlink:href="img/spritemap.svg#sprite-pass-hidden"></use></svg>';
+        }
+        else {
+            inputs.forEach(function(input) {
+                input.type = 'password';
+            });
+            this.innerHTML = '<svg><use xlink:href="img/spritemap.svg#sprite-pass-visible"></use></svg>';
+        }
+    });
+
+    // Tabs
+    if ($('#tabs').length) {
+        $('#tabs').tabs({
+            show: {
+                effect: 'fadeIn',
+                duration: 300,
+            },
+            hide: {
+                effect: 'fadeOut',
+                duration: 300,
+            },
+        });
+    }
+
+    // intl-tel-input
+    if ($('#phone').length) {
+        let input = document.querySelector("#phone");
+        let iti = intlTelInput(input, {
+            separateDialCode: true,
+            showSelectedDialCode: true,
+            utilsScript: "../../node_modules/intl-tel-input/build/js/utils.js",
+            initialCountry: "in",
+        });
+
+        // Обработчик события изменения страны
+        iti.promise.then(function () {
+            $("#countryCode").val(iti.getSelectedCountryData().dialCode);
+        });
+
+        // Обработчик события изменения телефонного номера
+        input.addEventListener("change", function () {
+            $("#countryCode").val(iti.getSelectedCountryData().dialCode);
+        });
+    }
+
     // Select2
     (function () {
         let selectStyled = $('.select2');
